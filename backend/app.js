ce5f0047f9aaa51421a9843109a437431a8d035b
Azur Kurbasic <azurkurbasic@gmail.com>
2023-08-10 22:31:26 +0200

    App.js

--- backend/app.js
+++ backend/app.js
@@ -9,10 +9,19 @@ const Property = require('./models/Property');
 
 app.use(cors());
 
+/**
+ * This is returning Promise<void>, then in line 25
+ * you're setting config.postgres.client to Promise<void>
+ * is this some kind of library configuration ?
+ */
 async function connectToPostgres() {
   const sequelize = new Sequelize(config.postgres.options);
   try {
     await sequelize.authenticate();
+    /**
+     * For real world apps I would always use more advanced logging, check
+     * what is available for NodeJS (Winston, Npmlog etc.)
+     */
     console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
@@ -25,6 +34,23 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
+/**
+ * It is good practice to keep your controller declarations clear
+ * for example here you are checking for req.query.page then setting default
+ * items_per_page and checking for conditions etc. In bigger projects always
+ * try to make that logic reusable, by creating some sort of functions that manage
+ * that for you, look at 'extractPaginationInfo' function. Now bottom code can be simplified, and
+ * 'extractPaginationInfo' function can be used in multiple places.
+ *
+ * Simplified version of this endpoint would look like this
+ * app.get('/api/properties', async (req, res) => {
+ *   const page = req.query.page;
+ *   const { offset, limit } = extractPaginationInfo(req)
+ *
+ *   const properties = await Property.findAll({limit, offset});
+ *   return res.json(properties).status(200);
+ * });
+ */
 app.get('/api/properties', async (req, res) => {
   const page = req.query.page;
   const items_per_page = 8;
@@ -39,6 +65,23 @@ app.get('/api/properties', async (req, res) => {
 
   return res.json(properties).status(200);
 });
+
+
+const extractPaginationInfo = (req, itemsPerPage = 8) => {
+  let page;
+  try {
+    page = parseInt(req.query.page ?? -1)
+  }catch (e) {
+    console.error(`Failed to cast string: ${req.qurey.page} to int, defaulting to -1`)
+    page = -1
+  }
+
+  return {
+    offset: page > 0 ? (page -1) * itemsPerPage : 0,
+    limit: itemsPerPage
+  }
+
+}
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
